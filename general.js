d431e86f7f24b0aa165b6b07e7321cc4e6032782
Albison Bekaj <albison.bekaj@student.uni-pr.edu>
2024-12-15 15:31:21 +0100

    Scripta per header dhe footer

---
+++ general.js
@@ -0,0 +1,23 @@
+async function loadPartial(partialUrl, placeholderId) {
+  try {
+    const response = await fetch(partialUrl);
+    const data = await response.text();
+    const placeholder = document.getElementById(placeholderId);
+    if (!placeholder) {
+      throw new Error(`Elementi me ID "${placeholderId}" nuk u gjet ne DOM.`);
+    }
+    placeholder.innerHTML = data;
+  } catch (error) {
+    console.error("Error ne loading e pjeseve te faqes", error);
+    const placeholder = document.getElementById(placeholderId);
+    if (placeholder) {
+      placeholder.innerHTML =
+        "<p>Ngarkimi i faqës dështoi. Ju lutemi kontaktoni mirëmbajtesin e Web faqës.</p>";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadPartial("navbar.html", "navbar-placeholder");
+  loadPartial("footer.html", "footer-placeholder");
+});
