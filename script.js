7a570958d67094f94322c6007b246cae7671770b
Dua-Gashi <dua.gashi@student.uni-pr.edu>
2025-01-04 18:57:10 +0100

    Logo

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ butonat.forEach(button => {
     });
 });
 
+
+const canvas = document.getElementById('logoCanvas');
+        const ctx = canvas.getContext('2d');
+
+        
+        ctx.fillStyle = '#3a5a40';
+        ctx.beginPath();
+        ctx.moveTo(25, 10); 
+        ctx.lineTo(45, 40); 
+        ctx.lineTo(5, 40); 
+        ctx.closePath();
+        ctx.fill();
+
+       
+        ctx.fillStyle = '#ffcc00';
+        ctx.beginPath();
+        ctx.arc(25, 15, 10, 0, Math.PI * 2, false);
+        ctx.fill();
+
