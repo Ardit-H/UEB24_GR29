3d516c2462fd4465a1c22a8e1dd11f9fdf17799c
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-01-06 16:42:05 +0100

    Permiresim ne kod

--- RrethNesh/rrethnesh.js
+++ RrethNesh/rrethnesh.js
@@ -6,7 +6,7 @@ function sendMail(){
     email : document.getElementById("email").value,
     message : document.getElementById("message").value,
     ID : document.getElementById("ID").value,
-   
+    
   }
 emailjs.send("service_zv6fa36","template_45ygsqv",parms).then(alert("Email sent !"))
 }
